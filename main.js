615b577d725176478dc4147557a0701c28c3538d
liuwenjun <570671435@qq.com>
2019-07-29 00:50:12 +0800

    2019-07-29 modify

---
+++ main.js
@@ -0,0 +1,14 @@
+var kb=[
+    [Q,W,E,R,T,Y,U,I,O,P],
+    [A,S,D,F,G,H,J,K,L],
+    [Z,X,C,V,B,N,M]
+];
+var kb_lc={
+    "Q":"qq.com",
+    "W":"weibo.com"
+}
+var kbd=document.getElementById("Keyboard");
+function tag(tagname){
+    document.createElement("tagname");
+};
+console.log(tag(div));
